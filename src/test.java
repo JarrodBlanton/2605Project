ad4c7c2f6697f050b514f018ee76ad12d94923f5
Jarrod Blanton <glassarrows09@gmail.com>
2014-10-24 11:54:16 -0400

    Added another test function. Added Jama folder with java files (non-compiled). Need to figure out why compiler won't recognize the Jama files.

--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 import Jama.Matrix;
 
+
 /**
  * Created by JarrodBlanton on 10/16/14.
  */
@@ -7,6 +8,9 @@ public class test {
     public static void main(String[] piratenoises) {
         System.out.println("jarrod i hate you, also tell me if you see this change.");
         createMatrix();
+        Matrix matrix = new Matrix(3, 3, 1.0);
+        System.out.println(sum(matrix));
+
     }
 
     public static void createMatrix() {
@@ -16,5 +20,15 @@ public class test {
         }
         Matrix a = new Matrix(arr);
         a.print(0, 0);
+    }
+
+    public static int sum(Matrix matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.getColumnDimension(); i++) {
+            for (int j = 0; i < matrix.getRowDimension(); j++) {
+                sum += matrix.get(i, j);
+            }
+        }
+        return sum;
     }
 }
