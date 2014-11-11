61551dcf57b3c56b8710d0afecf72d314457b0c3
JoelAnderson <joelbryceanderson@gmail.com>
2014-11-11 10:50:57 -0500

    Started gn_exp file

--- src/gn_exp.java
+++ src/gn_exp.java
@@ -1,6 +1,16 @@
+import Jama.Matrix;
+
 /**
  * Created by JarrodBlanton on 11/11/14.
  */
 public class gn_exp {
 
+    public gn_exp(double[] arr, double a, double b, double c, int iters) {
+        double[][] abc = new double[3][1];
+        abc[1][1] = a;
+        abc[2][1] = b;
+        abc[3][1] = c;
+        Matrix matrix = new Matrix(abc);
+
+    }
 }
