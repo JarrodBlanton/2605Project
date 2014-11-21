5eff5185b214007acb940d5f3619537d839df5ee
Jarrod Blanton <glassarrows09@gmail.com>
2014-11-21 00:05:04 -0500

    finished exp. make sure to test and look at the math for the rVals and jacobian vals loops.

--- src/gn_exp.java
+++ src/gn_exp.java
@@ -3,17 +3,34 @@ import Jama.Matrix;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 
 /**
  * Created by JarrodBlanton on 11/11/14.
  */
 public class gn_exp {
 
-    private Matrix arrValsMatrix;
-    private Matrix B;
-    public Matrix jacobianMatrix;
+    static Matrix rVals;
+    static Matrix B;
+    static Matrix jacobian;
 
-    public gn_exp(File file, double a, double b, double c, int iter) {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Please provide path to file");
+        String fileName = kb.nextLine();
+        File file = new File(fileName);
+        System.out.println("Provide initial guess for a");
+        double a = Double.parseDouble(kb.nextLine());
+        System.out.println("Provide initial guess for b");
+        double b = Double.parseDouble(kb.nextLine());
+        System.out.println("Provide initial guess for c");
+        double c = Double.parseDouble(kb.nextLine());
+        System.out.println("Provide number of iterations");
+        int iter = Integer.parseInt(kb.nextLine());
+        gn_exp(file, a, b, c, iter);
+    }
+
+    public static Matrix gn_exp(File file, double a, double b, double c, int iter) {
         double[][] abc = new double[3][1];
         double[] arr;
         abc[0][0] = a;
@@ -48,17 +65,17 @@ public class gn_exp {
             System.out.println("file not found");
             arr = new double[0];
         }
-        double[][] arrValsArray = new double[arr.length / 2][1];
 
+        double[][] rValsArray = new double[arr.length / 2][1];
         int count = 0;
         for (int i = 0; i < arr.length - 1; i += 2) {
             double x = arr[i];
             double y = arr[i + 1];
             double solution = y - (a * Math.exp(b * x) + c);
-            arrValsArray[count][0] = solution;
+            rValsArray[count][0] = solution;
             count++;
         }
-        arrValsMatrix = new Matrix(arrValsArray);
+        rVals = new Matrix(rValsArray);
 
         double[][] jValsArray = new double[arr.length / 2][3];
         count = 0;
@@ -72,6 +89,59 @@ public class gn_exp {
                     jValsArray[count][j] = -1;
                 }
             }
+            count++;
         }
+
+        jacobian = new Matrix(jValsArray);
+
+        for (int i = 0; i < iter; i++) {
+            // Step a
+            qr_fact_househ qrFact = new qr_fact_househ(jacobian);
+            Matrix Q = qrFact.getQMatrix();
+            Matrix R = qrFact.getRMatrix();
+            Matrix qT = Q.transpose();
+            Matrix qTR = qT.times(rVals);
+            double[][] rMatrix = R.getArrayCopy();
+            double[][] qtrVals = qTR.getArrayCopy();
+            // initialize x Array for back substitution
+            double[][] xVals = new double[3][1];
+            xVals[2][0] = qtrVals[2][0] / rMatrix[2][2];
+            xVals[1][0] = (qtrVals[1][0] - (rMatrix[1][2] * xVals[2][0])) / rMatrix[1][1];
+            xVals[0][0] = (qtrVals[0][0] - (rMatrix[0][1] * xVals[1][0]) - (rMatrix[0][2] * xVals[2][0])) / rMatrix[0][0];
+            Matrix xBack = new Matrix(xVals);
+            B = B.minus(xBack);
+
+            // Step b
+            abc = B.getArrayCopy();
+            a = abc[0][0];
+            b = abc[1][0];
+            c = abc[2][0];
+            count = 0;
+            for (int j = 0; j < arr.length - 1; j += 2) {
+                double x = arr[j];
+                double y = arr[j + 1];
+                double solution = y - (a * Math.exp(b * x) + c);
+                rValsArray[count][0] = solution;
+                count++;
+            }
+            rVals = new Matrix(rValsArray);
+
+            // Step c
+            count = 0;
+            for (int k = 0; k < arr.length - 1; k += 2) {
+                for (int l = 0; l < 3; l++) {
+                    if (l == 0) {
+                        jValsArray[count][l] = -1 * Math.exp(b * arr[k]);
+                    } else if (l == 1) {
+                        jValsArray[count][l] = -1 * arr[k] * Math.exp(b * arr[k]);
+                    } else {
+                        jValsArray[count][l] = -1;
+                    }
+                }
+                count++;
+            }
+            jacobian = new Matrix(jValsArray);
+        }
+        return B;
     }
 }
